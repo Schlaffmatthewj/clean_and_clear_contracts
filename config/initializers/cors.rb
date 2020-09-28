9bf0d2f8b5702426bbe5e6c83b6252d4873fe3be
Matthew Schlaff <schmayley@Matthews-Air.attlocal.net>
2020-09-28 10:48:55 -0600

    production or not

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
-        origins "https://fast-coast-80968.herokuapp.com/"
+        origins "https://fast-coast-80968.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, 
             :delete, :options, :head], credentials: true
     end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 if Rails.env == "production"
+    p 'production'
     Rails.application.config.session_store :cookie_store, 
-        key: "_contracts_authentication", domain: "https://fast-coast-80968.herokuapp.com/"
+        key: "_contracts_authentication", domain: "https://fast-coast-80968.herokuapp.com"
 else
+    p 'not production'
     Rails.application.config.session_store :cookie_store, 
         key: "_contracts_authentication"
 end
