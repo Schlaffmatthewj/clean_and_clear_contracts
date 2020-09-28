340e2a94b1aad43ae13ebf0a9000c8b42edfd474
Matthew Schlaff <schmayley@Matthews-Air.attlocal.net>
2020-09-28 10:52:57 -0600

    print cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,8 @@
 if Rails.env == "production"
-    p 'production'
+    p :cookie_store
     Rails.application.config.session_store :cookie_store, 
         key: "_contracts_authentication", domain: "https://fast-coast-80968.herokuapp.com"
 else
-    p 'not production'
     Rails.application.config.session_store :cookie_store, 
         key: "_contracts_authentication"
 end
